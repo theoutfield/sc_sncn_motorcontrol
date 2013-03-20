8f74889b7962e2b44dd917c5fc940e692b6082d0
Ludwig Orgler <lorgler@synapticon.com>
2013-03-20 19:25:11 +0100

    torque with speed control
    Change-Id: Ifa5e909c8f8c1fdf2cbbdc2647f5012df4ec8953

--- app_motorctrl/config/motor/dc_motor_config.h
+++ app_motorctrl/config/motor/dc_motor_config.h
@@ -14,13 +14,14 @@
 #pragma once
 
 #define UMOT_MAX 4096
-#define TORQUE_INTEGRATOR_MAX  UMOT_MAX*16
+#define TORQUE_INTEGRATOR_MAX  UMOT_MAX*32
 
-
+//#define defENCODER
+#define defHALL
 
 //=========================================================
-#define MOTOR_MAXON_EC45
-//#define MOTOR_MAXON_EC60
+//#define MOTOR_MAXON_EC45
+#define MOTOR_MAXON_EC60
 
 //==========================================================
 #ifdef  MOTOR_MAXON_EC45
@@ -30,10 +31,6 @@
 #define GEAR_RATIO	26
 #define MAX_NOMINAL_SPEED  4000   // in 1/min
 #define MAX_NOMINAL_CURRENT  5    // in A
-
-//#define defENCODER
-#define defHALL
-
 
 #define defParRpmMotorMax		 6500
 #define defParRpmUmotMax	 	11500
@@ -49,6 +46,7 @@
 #define defParUmotIntegralLimit	 2048
 #define defParPropGain			    0
 #define defParIntegralGain		    0
+#define defCurrentFactor            4
 
 #define defParTorquePropGain	   64
 #define defParTorqueIntegralGain   64
@@ -66,8 +64,9 @@
 #endif
 //=====================================================================
 
-
+//==================================================================
 #ifdef  MOTOR_MAXON_EC60
+
 #define MOTOR_POWER 100
 #define POLE_PAIRS	7
 #define GEAR_RATIO	156
@@ -82,12 +81,20 @@
 //======== current limits ===============
 #define defParRmsLimit			750   // 66*4 = 264Bits/A
 #define defParRmsMaxPwmOff      2000  //
+
 //============ speed_control ============
+#define defParEncoderResolution       4000
+#define defParEncoderZeroPointPlus     640
+#define defParEncoderZeroPointMinus   1100
+//-------------------------------------
+
 #define defParHysteresisPercent	    5
 #define defParDiffSpeedMax		  150
 #define defParUmotIntegralLimit	 2048
 #define defParPropGain			    8
 #define defParIntegralGain		    8
+#define defCurrentFactor            1
+
 
 #define defParTorquePropGain	   64
 #define defParTorqueIntegralGain   64
