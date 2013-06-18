08036dcea488d55c563737f87967a1b9d905aea8
Pavan Kanajar <pkanajar@synapticon.com>
2013-06-18 12:42:44 +0200

    hide channel_comm

--- test_velocity-ctrl/src/internal_config.h
+++ test_velocity-ctrl/src/internal_config.h
@@ -1,3 +1,7 @@
+#define VELOCITY_CTRL_ENABLE()   	c_velocity_ctrl <: 1
+#define VELOCITY_CTRL_DISABLE()  	c_velocity_ctrl <: 0
+#define VELOCITY_CTRL_READ(x)		c_velocity_ctrl :> x
+#define VELOCITY_CTRL_WRITE(x)		c_velocity_ctrl <: x
 
 #define SUCCESS 1
 #define ERROR   0 //based on timeout for success
