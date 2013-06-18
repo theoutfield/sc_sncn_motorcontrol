3bda9b5cae98a4d1a39081e5087b491fc292b7c6
Pavan Kanajar <pkanajar@synapticon.com>
2013-06-18 17:31:02 +0200

    add position_channnel_cmds

--- test_position-ctrl/src/internal_config.h
+++ test_position-ctrl/src/internal_config.h
@@ -3,6 +3,11 @@
 #define VELOCITY_CTRL_READ(x)		c_velocity_ctrl :> x
 #define VELOCITY_CTRL_WRITE(x)		c_velocity_ctrl <: x
 
+#define POSITION_CTRL_ENABLE()   	c_position_ctrl <: 1
+#define POSITION_CTRL_DISABLE()  	c_position_ctrl <: 0
+#define POSITION_CTRL_READ(x)		c_position_ctrl :> x
+#define POSITION_CTRL_WRITE(x)		c_position_ctrl <: x
+
 #define SET    1
 #define UNSET  0
 
