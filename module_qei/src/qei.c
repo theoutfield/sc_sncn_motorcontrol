261912aa62adac8f3135b28a92e6b31c021e7f26
Pavan Kanajar <pkanajar@synapticon.com>
2013-08-01 18:15:54 +0200

    add convert function

---
+++ module_qei/src/qei.c
@@ -0,0 +1,18 @@
+/*
+ * qei.c
+ *
+ *  Created on: Aug 1, 2013
+ *      Author: pkanajar
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+int __qei_max_counts(int real_counts)
+{
+	int max_counts;
+	double max_counts_power = log10( (double)real_counts)/log10(2.0);
+	max_counts = (int) round(max_counts_power);
+	max_counts = 1 << (max_counts);
+	return max_counts;
+}
