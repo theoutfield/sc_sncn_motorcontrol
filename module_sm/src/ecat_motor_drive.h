943c0f221559a8579afdbd6afcf00a6e238dc172
Pavan Kanajar <pkanajar@synapticon.com>
2013-10-14 09:47:19 +0200

    add comments and clean-up module_sm

--- module_sm/src/ecat_motor_drive.h
+++ module_sm/src/ecat_motor_drive.h
@@ -17,6 +17,25 @@
 #include <qei_server.h>
 #include <profile.h>
 
+/**
+ * \brief This server implementation enables motor drive functions via Ethercat communication
+ *
+ *  Input Channel
+ * \channel pdo_in channel to receive information from ethercat
+ * \channel coe_out channel to receive motor config information from ethercat
+ * \channel c_signal channel to receive init ack from commutation loop
+ * \channel c_hall channel to receive position information from hall
+ * \channel c_qei channel to receive position information from qei
+ * \channel c_adc channel to receive torque information from adc
+ *
+ *  Output Channel
+ * \channel pdo_out channel to send out information via ethercat
+ * \channel c_position_ctrl channel to receive actual position
+ * \channel c_torque_ctrl channel to receive/send torque control information
+ * \channel c_velocity_ctrl channel to receive/send velocity control information
+ * \channel c_position_ctrl channel to receive/send position control information
+ *
+ */
 void ecat_motor_drive(chanend pdo_out, chanend pdo_in, chanend coe_out, chanend c_signal, chanend c_hall_p4,\
 		chanend c_qei_p4, chanend c_adc, chanend c_torque_ctrl, chanend c_velocity_ctrl, chanend c_position_ctrl);
 
