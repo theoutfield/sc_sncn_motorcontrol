494de323e8e1742747a1907ac22d1d3e547f5b6d
Pavan Kanajar <pkanajar@synapticon.com>
2014-03-21 16:22:10 +0100

    update comments

--- module_gpio/src/gpio_client.h
+++ module_gpio/src/gpio_client.h
@@ -2,11 +2,51 @@
 #include <internal_config.h>
 #include <platform.h>
 
+/**
+ * 	\fn config_gpio(chanend c_gpio, int port_number, int switch_type)
+ *  \brief Client enables configuration of digital I/Os as inputs or outputs,
+ *  	   to read input ports and write to output ports.
+ *  	   By default all IO ports act as outputs if not configured as inputs.
+ *
+ *  \param c_gpio channel used to configure any digital ports /and send commands  to read/write to port specified
+ *  \param port_number selects a port to be configured: 0 - EXT_D0, 1 - EXT_D1, 2 - EXT_D2, 3 - EXT_D3, 4 - all EXT_D0, EXT_D1, EXT_D2, EXT_D3
+ *  \param switch_type specifies type: Active High/Active Low when switch is closed
+ *
+ *  \return 1 - success
+ *
+ */
 int config_gpio_digital_input(chanend c_gpio, int port_number, int input_type, int switch_type);
 
+/**
+ * 	\fn end_config_gpio(chanend c_gpio)
+ *  \brief Disables further configuration of digital I/Os as inputs or outputs.
+ *
+ *  \param c_gpio channel used to configure any digital ports
+ *  \return 1 - success
+ *
+ */
 int end_config_gpio(chanend c_gpio);
 
+/**
+ * 	\fn read_gpio_digital_input(chanend c_gpio, int port_number)
+ *  \brief Read digital input ports
+ *
+ *  \param c_gpio channel used to read ports
+ *  \param port_number selects a port to be read  0 - EXT_D0, 1 - EXT_D1, 2 - EXT_D2, 3 - EXT_D3, 4 - all EXT_D0, EXT_D1, EXT_D2, EXT_D3
+ *
+ *  \return port value
+ *
+ */
 int read_gpio_digital_input(chanend c_gpio, int port_number);
 
+/**
+ * 	\fn write_gpio_digital_output(chanend c_gpio, int port_number, int port_value)
+ *  \brief Write to digital ports
+ *
+ *  \param c_gpio channel used to write ports
+ *  \param port_number selects a port to write: 0 - EXT_D0, 1 - EXT_D1, 2 - EXT_D2, 3 - EXT_D3, 4 - all EXT_D0, EXT_D1, EXT_D2, EXT_D3
+ *	\param port_value specify a value: 1/0 to drive port
+ *
+ */
 void write_gpio_digital_output(chanend c_gpio, int port_number, int port_value);
 
--- module_gpio/src/gpio_client.xc
+++ module_gpio/src/gpio_client.xc
@@ -1,19 +1,6 @@
 #include <gpio_client.h>
 #include <xscope.h>
 
-/**
- * 	\fn config_gpio(chanend c_gpio, int port_number, int switch_type)
- *  \brief Client enables configuration of digital I/Os as inputs or outputs,
- *  	   to read input ports and write to output ports.
- *  	   By default all IO ports act as outputs if not configured as inputs.
- *
- *  \param c_gpio channel used to configure any digital ports /and send commands  to read/write to port specified
- *  \param port_number selects a port to be configured: 0 - EXT_D0, 1 - EXT_D1, 2 - EXT_D2, 3 - EXT_D3, 4 - all EXT_D0, EXT_D1, EXT_D2, EXT_D3
- *  \param switch_type specify the input
- *
- *  \return 1 - success
- *
- */
 int config_gpio_digital_input(chanend c_gpio, int port_number, int input_type, int switch_type)
 {
 	int config_state = 1;
@@ -30,34 +17,12 @@ int config_gpio_digital_input(chanend c_gpio, int port_number, int input_type, i
 	return config_state;
 }
 
-/**
- * 	\fn end_config_gpio(chanend c_gpio)
- *  \brief Disables further configuration of digital I/Os as inputs or outputs.
- *
- *  \param c_gpio channel used to configure any digital ports
- *  \return 1 - success
- *
- */
 int end_config_gpio(chanend c_gpio)
 {
 	c_gpio <: CONFIG_DIO_DONE;
 	return 1;
 }
 
-/**
- * 	\fn config_gpio(chanend c_gpio, int port_number, int switch_type)
- *  \brief Client enables configuration of digital I/Os as inputs or outputs,
- *  	   to read input ports and write to output ports.
- *  	   By default all IO ports act as outputs if not configured as inputs.
- *
- *  \param c_gpio channel used to configure any digital ports /and send commands  to read/write to port EXT_D0
- *  	    0 - EXT_D0
- *  \param port_number selects a port to be configured
- *  \param switch_type specify the input
- *
- *  \return 1 - success
- *
- */
 int read_gpio_digital_input(chanend c_gpio, int port_number)
 {
 	int port_value;
