bd9d4e1db4d6e5704ccbd9a0ee5e09646589d4d9
Agus <atena@synapticon.com>
2015-10-01 17:30:07 +0200

    Changing PWM config. It should work on all the boards.

--- module_motorcontrol_common/include/pwm_config.h
+++ module_motorcontrol_common/include/pwm_config.h
@@ -5,13 +5,14 @@
 /* All timing values depend on reference clock setting. For 100 MHz reference clock each tick
  * is equivalent to 10ns (for 250 MHz clock it's 4ns)
  */
-#define PWM_DEAD_TIME 300 //750 for DC1K, 300 for the rest
+#define PWM_DEAD_TIME 750
 
 /* This defines the PWM resolution. Because the PWM is driven by a fixed clock,
  * this also configures the PWM frequency at the same time. The relation is as follows:
  * PWM_MAX_VALUE = PWM_CLOCK / PWM_FREQUENCY (e.g. 250 MHz / 18 kHz = 13889)
  */
-#define PWM_MAX_VALUE 13889 // 12500 for DC1K, 13889 for the rest
+#define PWM_MAX_VALUE 12500
+
 
 /* Define if ADC sampling is locked to PWM switching. The ADC sampling will occur in the middle of
  * the switching sequence. It is triggered over a channel. Set this define to 0 to disable this feature
