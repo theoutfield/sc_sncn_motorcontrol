fa7cb2f41124688b4206f456386d52d1d6a75955
Agus <atena@synapticon.com>
2015-11-19 10:49:45 +0100

    QEI fully adapted. All apps working. Maximum speed measurement of 16000 ticks * 900 RPM

--- module_qei/include/qei_server.h
+++ module_qei/include/qei_server.h
@@ -6,7 +6,56 @@
 
 #pragma once
 
-#include <qei_config.h>
+//#include <qei_config.h>
+#define FILTER_LENGTH_QEI        8
+#define FILTER_LENGTH_QEI_PWM    8
+
+#define QEI_RPM_CONST            1000*60
+#define QEI_PWM_RPM_CONST        18000*60
+
+#define QEI_RAW_POS_REQ          1
+#define QEI_ABSOLUTE_POS_REQ     2
+#define QEI_VELOCITY_REQ         3
+#define QEI_VELOCITY_PWM_RES_REQ 4
+#define SYNC                     5
+#define SET_OFFSET               6
+#define QEI_RESET_COUNT          7
+
+/**
+ * @brief struct definition for quadrature sensor
+ */
+typedef struct {
+    int max_ticks_per_turn;
+    int real_counts;
+    int max_ticks;      // paramater allows for more turns
+    int index;          // no_index - 0 index - 1
+    int poles;
+    int sensor_polarity;
+} qei_par;
+
+/**
+ * @brief struct definition for velocity calculation from qei sensor
+ */
+typedef struct QEI_VELOCITY_PARAM
+{
+    int previous_position;
+    int old_difference;
+    int filter_buffer[8];
+    int index;
+    int filter_length;
+} qei_velocity_par;
+
+
+/**
+ * @brief Internal function to calculate QEI position information
+ *
+ * @param real_counts qei counts per rotation
+
+ * @return  max position from qei sensor
+ */
+extern int __qei_max_counts(int real_counts);
+
+#ifdef __XC__
 
  /**
 * @brief Structure containing hall sensor port/s
@@ -27,7 +76,6 @@ interface QEIInterface{
 
 };
 
-//int calculate_qei_velocity(int qei_absolute_position, qei_par &qei_params, qei_velocity_par &qei_velocity_params);
 
 /**
  * @brief initialize QEI sensor
@@ -60,3 +108,4 @@ int calculate_qei_velocity(int count, qei_par &qei_config, qei_velocity_par &qei
 void run_qei(interface QEIInterface server i_qei[5],
              EncoderPorts & encoder_ports, qei_par qei_config, qei_velocity_par qei_velocity_params);
 
+#endif
