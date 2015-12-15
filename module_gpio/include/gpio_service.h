71014533b66a3b44bc101255c77b22e052488d12
Begona Alvarez <begona.alvarezd@gmail.com>
2015-12-15 17:52:22 +0100

    Initializing documentation for module_gpio

--- module_gpio/include/gpio_service.h
+++ module_gpio/include/gpio_service.h
@@ -10,10 +10,38 @@
 #include <internal_config.h>
 #include <platform.h>
 
+/**
+ * @brief Lorem ipsum...
+ */
 interface GPIOInterface{
+	/**
+     * @brief Lorem ipsum...
+     *
+     * @param gpio_port Lorem ipsum...
+     * @param input_type Lorem ipsum...
+     * @param switch_type Lorem ipsum...
+     *
+     * @return Lorem ipsum...
+     */
     int config_dio_input(int gpio_port, int input_type, int switch_type);
+    /**
+     * @brief Lorem ipsum...
+     */
     void config_dio_done();
+    /**
+     * @brief Lorem ipsum...
+     *
+     * @param gpio_port Lorem ipsum...
+     *
+     * @return Lorem ipsum...
+     */
     int read_gpio(int gpio_port);
+    /**
+     * @brief Lorem ipsum...
+     *
+     * @param gpio_port Lorem ipsum...
+     * @param value Lorem ipsum...
+     */
     void write_gpio(int gpio_port, int value);
 };
 /**
@@ -21,18 +49,9 @@ interface GPIOInterface{
  * chanend c_gpio_0, chanend c_gpio_1, chanend c_gpio_2, chanend c_gpio_3)
  * @brief Server enables configuration of digital I/Os as inputs or outputs,
  *  	   to read input ports and write to output ports
- * @param p_ifm_ext_d0 Digital I/O port EXT_D0
- * @param p_ifm_ext_d1 Digital I/O port EXT_D1
- * @param p_ifm_ext_d2 Digital I/O port EXT_D2
- * @param p_ifm_ext_d3 Digital I/O port EXT_D3
- * @param c_gpio_0 channel used for configuration of any digital ports and send commands
- *  	   to read/write to port EXT_D0
- * @param c_gpio_1 channel used for configuration of any digital ports and send commands
- *  	   to read/write to port EXT_D1
- * @param c_gpio_2 channel used for configuration of any digital ports and send commands
- *  	   to read/write to port EXT_D2
- * @param c_gpio_3 channel used for configuration of any digital ports and send commands
- *  	   to read/write to port EXT_D3
+ *
+ * @param gpio_ports[4] Lorem ipsum...
+ * @param i_gpio[2] Lorem ipsum...
  *
  */
 void gpio_service(port gpio_ports[4], interface GPIOInterface server i_gpio[2]);
