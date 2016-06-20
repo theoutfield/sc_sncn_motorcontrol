73d3254abf2ab3576bc4b7617295aeb784bf6a0e
vkhorani <vkhorani@synapticon.com>
2016-06-20 16:56:56 +0200

    tune j5 on foresight

--- examples/config_motor/user_config.h
+++ examples/config_motor/user_config.h
@@ -375,20 +375,48 @@
 
 // Foresight: Joint 3 Position Controller Config
 //**motor offset: 300
+//#define MIN_POSITION_LIMIT     -1500000         // ticks (min range: -2^30, limited for safe operation)
+//#define MAX_POSITION_LIMIT      1500000         // ticks (max range: 2^30, limited for safe operation)
+//#define MAX_VELOCITY            1200             // rpm
+//#define MAX_TORQUE              1200000
+//
+//#define POSITION_Kp             170
+//#define POSITION_Ki             35
+//#define POSITION_Kd             0
+//#define VELOCITY_Kp             38
+//#define VELOCITY_Ki             0
+//#define VELOCITY_Kd             35
+//
+//#define POSITION_P_ERROR_lIMIT  40000
+//#define POSITION_I_ERROR_lIMIT  5
+//#define POSITION_INTEGRAL_LIMIT 25000
+//#define VELOCITY_P_ERROR_lIMIT  150000
+//#define VELOCITY_I_ERROR_lIMIT  0
+//#define VELOCITY_INTEGRAL_LIMIT 0
+//#define POSITION_REF_FC         140
+//#define POSITION_FC             80
+//#define VELOCITY_REF_FC         100
+//#define VELOCITY_FC             80
+//#define VELOCITY_D_FC           60
+
+
+
+// Foresight: Joint 5 Position Controller Config
+//**motor offset: 1665
 #define MIN_POSITION_LIMIT     -1500000         // ticks (min range: -2^30, limited for safe operation)
 #define MAX_POSITION_LIMIT      1500000         // ticks (max range: 2^30, limited for safe operation)
 #define MAX_VELOCITY            1200             // rpm
 #define MAX_TORQUE              1200000
 
-#define POSITION_Kp             170
-#define POSITION_Ki             35
+#define POSITION_Kp             210
+#define POSITION_Ki             40
 #define POSITION_Kd             0
-#define VELOCITY_Kp             38
+#define VELOCITY_Kp             25
 #define VELOCITY_Ki             0
-#define VELOCITY_Kd             35
+#define VELOCITY_Kd             15
 
 #define POSITION_P_ERROR_lIMIT  40000
-#define POSITION_I_ERROR_lIMIT  5
+#define POSITION_I_ERROR_lIMIT  4
 #define POSITION_INTEGRAL_LIMIT 25000
 #define VELOCITY_P_ERROR_lIMIT  150000
 #define VELOCITY_I_ERROR_lIMIT  0
