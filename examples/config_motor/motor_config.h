0e7a0252d70eb95171dbabdef75326ddb6eb55a7
Ramin <rsalehi@synapticon.com>
2016-09-06 17:12:04 +0200

    add settings to work with DC100

--- examples/config_motor/motor_config.h
+++ examples/config_motor/motor_config.h
@@ -81,6 +81,27 @@
 */
 
 
+// 411678 MAXON motor
+
+// IMPORTANT PARAMETERS (=> lead to mulfunction or damage if set wrong)
+#define POLE_PAIRS              7      //number of motor pole-pairs
+#define PERCENT_TORQUE_CONSTANT 5      //motor torque constant multiplied by 100
+#define RATED_CURRENT           5470        //rated phase current [milli-Amp-RMS]
+#define MAXIMUM_TORQUE          2890       //maximum value of torque which can be produced by motor [milli-Nm]
+#define RATED_TORQUE            289        // rated motor torque [milli-Nm]
+
+// OTHER PARAMETERS (do not change if not having access to the following parameter values)
+#define RATED_POWER          100        // rated power [W]
+#define PEAK_SPEED          4000        // maximum motor speed [rpm]
+#define PHASE_RESISTANCE    152000      // motor phase resistance [micro-ohm]
+#define PHASE_INDUCTANCE    188000      // motor phase inductance [micro-Hunnry]
+
+// GENERAL PARAMETERS
+#define MOTOR_TYPE              BLDC_MOTOR      //MOTOR TYPE [BLDC_MOTOR, BDC_MOTOR]
+#define BLDC_WINDING_TYPE       STAR_WINDING    //MOTOR TYPE [BLDC_MOTOR, BDC_MOTOR]
+
+
+/*
 // motor model: DT4
 
 // IMPORTANT PARAMETERS (=> lead to mulfunction or damage if set wrong)
@@ -98,7 +119,7 @@
 
 // GENERAL PARAMETERS
 #define MOTOR_TYPE              BLDC_MOTOR      //MOTOR TYPE [BLDC_MOTOR, BDC_MOTOR]
-
+*/
 
 /*
 // motor model: MABI AXIS_1 and AXIS_2
