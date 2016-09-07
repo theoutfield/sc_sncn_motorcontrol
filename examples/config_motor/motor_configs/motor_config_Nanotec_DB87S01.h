4f0ed69015132cb0d26a671deda143687d87383d
Romuald Junot <rjunot@synapticon.com>
2016-09-07 18:40:34 +0200

    update motor configs

--- examples/config_motor/motor_configs/motor_config_Nanotec_DB87S01.h
+++ examples/config_motor/motor_configs/motor_config_Nanotec_DB87S01.h
@@ -10,7 +10,7 @@
 
 // IMPORTANT PARAMETERS (=> lead to mulfunction or damage if set wrong)
 #define POLE_PAIRS              4       //number of motor pole-pairs
-#define PERCENT_TORQUE_CONSTANT (MAXIMUM_TORQUE/10)      //motor torque constant multiplied by 100
+#define PERCENT_TORQUE_CONSTANT 10      //motor torque constant multiplied by 100
 #define RATED_CURRENT           6250    //rated phase current (RMS)
 #define MAXIMUM_TORQUE          2128    //maximum value of torque which can be produced by motor [milli-Nm]
 #define RATED_TORQUE            700     // rated motor torque [milli-Nm]
--- examples/config_motor/motor_configs/motor_config_qmot_qbl5704.h
+++ examples/config_motor/motor_configs/motor_config_qmot_qbl5704.h
@@ -1,5 +1,5 @@
 /**
- * @file motor_config_Nanotec_DB42S03.h
+ * @file motor_config_qmot_qbl5704.h
  * @brief Motor Control config file (define your motor specifications here)
  * @author Synapticon GmbH <support@synapticon.com>
  */
@@ -8,74 +8,18 @@
  *********      USER CONFIGURATION       **********
  **************************************************/
 
-/*
- *  Motor specific offst settings
- */
+// IMPORTANT PARAMETERS (=> lead to mulfunction or damage if set wrong)
+#define POLE_PAIRS              2       //number of motor pole-pairs
+#define PERCENT_TORQUE_CONSTANT 10      //motor torque constant multiplied by 100
+#define RATED_CURRENT           14500   //rated phase current [milli-Amp-RMS]
+#define MAXIMUM_TORQUE          3000    //maximum value of torque which can be produced by motor [milli-Nm]
 
-#define REGEN_V_HIGH          58
-#define REGEN_V_LOW           38
+// OTHER PARAMETERS (do not change if not having access to the following parameter values)
+#define RATED_POWER             176     // rated power [W]
+#define RATED_TORQUE            420     // rated motor torque [milli-Nm]
+#define PEAK_SPEED              4000    // maximum motor speed [rpm]
+#define PHASE_RESISTANCE        350000 // motor phase resistance [micro-ohm]
+#define PHASE_INDUCTANCE        1000    // motor phase inductance [micro-Hunnry]
 
-#define REGEN_I_HIGH          60
-#define REGEN_I_LOW           0
-
-#define PERCENT_CHARGING_FACTOR_MAX  52
-#define PERCENT_CHARGING_FACTOR_MIN  1
-
-
-/*
- * Set the Motor offset and position of the hall state sensors.
- * Unless for adding a new motor or fixing motor settings do not touch this.
- */
-#define MOTOR_OFFSET           0
-
-#define MOTOR_HALL_STATE_1     0
-#define MOTOR_HALL_STATE_2     0
-#define MOTOR_HALL_STATE_3     0
-#define MOTOR_HALL_STATE_4     0
-#define MOTOR_HALL_STATE_5     0
-#define MOTOR_HALL_STATE_6     0
-
-#define POWER_FACTOR           0
-#define POLE_PAIRS             2
-
-
-/////////////////////////////////////////////
-//////  MOTOR PARAMETERS
-////////////////////////////////////////////
-
-// RATED POWER
-#define RATED_POWER 140         // W
-
-// RATED TORQUE
-#define RATED_TORQUE 270        // mNm
-
-// TORQUE CONSTANT
-#define PERCENT_TORQUE_CONSTANT     15
-
-// RATED CURRENT
-#define RATED_CURRENT 5         // Amp
-
-// PEAK SPEED
-#define PEAK_SPEED    9000      // rpm
-
-// PHASE RESISTANCE
-#define PHASE_RESISTANCE 350000 // uOhm
-
-// PHASE INDUCTANCE
-#define PHASE_INDUCTANCE 1000    // uH
-
-// MAXIMUM TORQUE
-#define MAXIMUM_TORQUE   63    //maximum torque (Nm) at 1000 Amp
-
-/*********************************************/
-
-/////////////////////////////////////////////
-//////  GENERAL MOTOR CONFIGURATION
-////////////////////////////////////////////
-
-// MOTOR TYPE [BLDC_MOTOR, BDC_MOTOR]
-#define MOTOR_TYPE  BLDC_MOTOR
-
-// WINDING TYPE (if applicable) [STAR_WINDING, DELTA_WINDING]
-#define BLDC_WINDING_TYPE   STAR_WINDING
-
+// GENERAL PARAMETERS
+#define MOTOR_TYPE              BLDC_MOTOR      //MOTOR TYPE [BLDC_MOTOR, BDC_MOTOR]
