aa8d813a097f981569e867d43ff5b91fe57d144a
Romuald Junot <rjunot@synapticon.com>
2016-09-27 19:57:55 +0200

    add advanced_motorcontrol_licence.h in config_motor and remove module_motorcontrol_licence dependency

---
+++ examples/config_motor/advanced_motorcontrol_licence.h
@@ -0,0 +1,15 @@
+/*
+ * advanced_motorcontrol_licence.h
+ *
+ *  Created on: Aug 3, 2016
+ *      Author: ramin
+ */
+
+#pragma once
+
+/* ***********************************************************************
+ * Change this to the real licence number in module_motorcontrol_licence.
+ * Don't push the real number to a public repository!
+ * **********************************************************************/
+
+#define ADVANCED_MOTOR_CONTROL_LICENCE 0
