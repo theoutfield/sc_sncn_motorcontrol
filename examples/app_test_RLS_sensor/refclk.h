975ba2e40065c8b2f97c3d536e830820314e3e56
Rawia Abed <rabed@synapticon.com>
2016-12-20 16:48:35 +0100

    app_test_RLS_sensor: initial commit

---
+++ examples/app_test_RLS_sensor/refclk.h
@@ -0,0 +1,40 @@
+/**
+ * @file refclk.h
+ * @brief Reference Clock definitions
+ * @author Synapticon GmbH <support@synapticon.com>
+ */
+
+
+#pragma once
+
+/**
+ * Defines the amount of ticks of a 100MHz timer (standard system reference frequency) for 1 us.
+ */
+#define USEC_STD       100
+
+/**
+ * Defines the amount of ticks of a 100MHz timer (standard system reference frequency) for 1 ms.
+ */
+#define MSEC_STD    100000
+
+/**
+ * Defines the amount of ticks of a 100MHz timer (standard system reference frequency) for 1 s.
+ */
+#define SEC_STD  100000000
+
+/**
+ * Defines the amount of ticks of a 250MHz timer (Motorcontrol TILE reference frequency) for 1 us.
+ */
+#define USEC_FAST       250
+
+/**
+ * Defines the amount of ticks of a 250MHz timer (Motorcontrol TILE reference frequency) for 1 ms.
+ */
+
+#define MSEC_FAST    250000
+
+/**
+ * Defines the amount of ticks of a 250MHz timer (Motorcontrol TILE reference frequency) for 1 s.
+ */
+#define SEC_FAST  250000000
+
---
+++ examples/app_test_RLS_sensor/src/app_test_RLS_sensor.xc
@@ -0,0 +1,36 @@
+#include <CORE_C22-rev-a.bsp>
+
+//#include <stdint.h>
+//#include <inttypes.h>
+//#include <time.h> /* has to be here because clock type gives a clash with the type in xs1.h */
+
+in port P8C = on tile[IFM_TILE]: XS1_PORT_8C;
+
+int a = 0,b = 0,c = 0,d = 0,e = 0,f = 0,g = 0,h = 0;
+int data = 0;
+inline static void delay_ten_nanosecond(timer t, int delay)
+{
+    int ts;
+    t:>ts;
+    t when timerafter(ts+delay) :> void;
+}
+
+int main(void)
+{
+    par {
+        on tile[IFM_TILE]: {
+
+            delay_milliseconds(2);
+
+            while (1)
+            {
+
+                P8C :> data;
+                printstr("Position = "); printintln(data);
+            }
+
+        }
+    }
+
+    return 0;
+}
