c48ad7ee8b1ff02ed03b552975bf2f19901c147e
Ramin <rsalehi@synapticon.com>
2017-01-30 15:46:03 +0100

    create app_demo_adc

---
+++ examples/app_demo_adc/src/tuning.h
@@ -0,0 +1,29 @@
+/*
+ * tuning.h
+ *
+ *  Created on: Jul 13, 2015
+ *      Author: Synapticon GmbH
+ */
+
+
+#pragma once
+
+#include <platform.h>
+#include <motor_control_interfaces.h>
+#include <advanced_motor_control.h>
+#include <advanced_motorcontrol_licence.h>
+#include <refclk.h>
+#include <adc_service.h>
+#include <position_ctrl_service.h>
+
+#include <xscope.h>
+//#include <bldc_motor_config.h>
+#include <mc_internal_constants.h>
+#include <user_config.h>
+
+interface PositionLimiterInterface {
+    void set_limit(int limit);
+    int get_limit();
+};
+
+void demo_torque_position_velocity_control(client interface PositionVelocityCtrlInterface i_position_control);
