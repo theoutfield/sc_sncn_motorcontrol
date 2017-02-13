f038d9a23dfddaea5928334bcdd786100ba80022
Henrik Strötgen <hstroetgen@synapticon.com>
2017-02-13 12:33:34 +0100

    motor_config_Nanotec_DB42C02.h: Add missing const
    Add missing constant values (torque, resistance, inductance,...)

--- examples/config_motor/motor_configs/motor_config_Nanotec_DB42C02.h
+++ examples/config_motor/motor_configs/motor_config_Nanotec_DB42C02.h
@@ -12,11 +12,19 @@
 //////  GENERAL MOTOR CONFIGURATION
 ////////////////////////////////////////////
 
-// MOTOR TYPE [BLDC_MOTOR, BDC_MOTOR]
-#define MOTOR_TYPE  BLDC_MOTOR
 
-// NUMBER OF POLE PAIRS (if applicable)
-#define POLE_PAIRS  4
+// IMPORTANT PARAMETERS (=> lead to mulfunction or damage if set wrong)
+#define POLE_PAIRS              4      //number of motor pole-pairs
+#define PERCENT_TORQUE_CONSTANT 3      //motor torque constant multiplied by 100
+#define RATED_CURRENT           3570   //rated phase current [milli-Amp-RMS]
+#define MAXIMUM_TORQUE          300    //maximum value of torque which can be produced by motor [milli-Nm]
+#define RATED_TORQUE            100    //rated motor torque [milli-Nm].
 
-// WINDING TYPE (if applicable) [STAR_WINDING, DELTA_WINDING]
-#define BLDC_WINDING_TYPE   DELTA_WINDING
+// OTHER PARAMETERS (do not change if not having access to the following parameter values)
+#define RATED_POWER             147     // rated power [W]
+#define PEAK_SPEED              14000   // maximum motor speed [rpm]
+#define PHASE_RESISTANCE        200000  // motor phase resistance [micro-ohm]
+#define PHASE_INDUCTANCE        320     // motor phase inductance [micro-Henry]
+
+// GENERAL PARAMETERS
+#define MOTOR_TYPE              BLDC_MOTOR      //MOTOR TYPE [BLDC_MOTOR, BDC_MOTOR]
