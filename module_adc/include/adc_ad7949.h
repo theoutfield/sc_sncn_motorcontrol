fff5b9edc01b27eb261d942918a2a44bb679d163
Ramin <rsalehi@synapticon.com>
2017-02-15 11:04:32 +0100

    properly comment the file adc_ad7949.h

--- module_adc/include/adc_ad7949.h
+++ module_adc/include/adc_ad7949.h
@@ -11,19 +11,16 @@
 #include <adc_service.h>
 
 /**
- * @brief Non triggered ADC server
+ * @brief Service to sample analogue inputs of ADC module
  *
+ * @param iADC[2]               Interface to communicate with clients and send the measured values
+ * @param adc_ports             Structure type to manage the AD7949 ADC chip.
+ * @param current_sensor_config Structure type to calculate the proper sign (positive/negative) of sampled phase currents
+ * @param i_watchdog            Interface to communicate with watchdog service
+ * @param operational_mode      Reserved
  *
- * This server should be used if the SOMANET node is not used for motor
- * drive/control. This is the interface to AD7949 ADC devices. It controls
- * two devices so that two channels can be sampled simultaneously.
- *
- * @param i_adc Interface to receive ADC output
- * @param adc_ports Structure containing hardware ports (e.g. SPI) to the ADC
- * @param current_sensor_config Structure containing configurations for this service
- *
+ * @return void
  */
-
 void adc_ad7949(
         interface ADCInterface server i_adc[2],
         AD7949Ports &adc_ports,
