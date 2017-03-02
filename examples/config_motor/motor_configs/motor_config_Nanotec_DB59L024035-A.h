314efe6750053a9e69bb6cd5b3e23a56f577b0a5
Romuald Junot <rjunot@synapticon.com>
2017-03-02 12:54:39 +0100

    update motor config for Nanotec_DB59L024035-A

--- examples/config_motor/motor_configs/motor_config_Nanotec_DB59L024035-A.h
+++ examples/config_motor/motor_configs/motor_config_Nanotec_DB59L024035-A.h
@@ -4,19 +4,18 @@
  * @author Synapticon GmbH <support@synapticon.com>
  */
 
-/**************************************************
- *********      USER CONFIGURATION       **********
- **************************************************/
+// IMPORTANT PARAMETERS (=> lead to mulfunction or damage if set wrong)
+#define POLE_PAIRS              6       //number of motor pole-pairs
+#define PERCENT_TORQUE_CONSTANT 5       //motor torque constant multiplied by 100
+#define RATED_CURRENT           9400    //rated phase current [milli-Amp-RMS]
+#define MAXIMUM_TORQUE          1410    //maximum value of torque which can be produced by motor [milli-Nm]
+#define RATED_TORQUE            470     // rated motor torque [milli-Nm]
 
-/////////////////////////////////////////////
-//////  GENERAL MOTOR CONFIGURATION
-////////////////////////////////////////////
+// OTHER PARAMETERS (do not change if not having access to the following parameter values)
+#define RATED_POWER             172     // rated power [W]
+#define PEAK_SPEED              4500    // maximum motor speed [rpm]
+#define PHASE_RESISTANCE        220000  // motor phase resistance [micro-ohm]
+#define PHASE_INDUCTANCE        290     // motor phase inductance [micro-Henry]
 
-// MOTOR TYPE [BLDC_MOTOR, BDC_MOTOR]
-#define MOTOR_TYPE  BLDC_MOTOR
-
-// NUMBER OF POLE PAIRS (if applicable)
-#define POLE_PAIRS  3
-
-// WINDING TYPE (if applicable) [STAR_WINDING, DELTA_WINDING]
-#define BLDC_WINDING_TYPE   DELTA_WINDING // In datasheet of Nanotec: Star winding, but this is not working
+// GENERAL PARAMETERS
+#define MOTOR_TYPE              BLDC_MOTOR      //MOTOR TYPE [BLDC_MOTOR, BDC_MOTOR]
