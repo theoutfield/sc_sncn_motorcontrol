e0b3f10c412f7f165c06afa70d693df83e5e6596
Ramin <rsalehi@synapticon.com>
2017-03-28 13:48:40 +0200

    create app_demo_motion_control

---
+++ examples/app_demo_motion_control/src/user_interface_service.h
@@ -0,0 +1,26 @@
+/*
+ * tuning.h
+ *
+ *  Created on: Jul 13, 2015
+ *      Author: Synapticon GmbH
+ */
+
+
+#pragma once
+
+#include <platform.h>
+#include <motor_control_interfaces.h>
+#include <advanced_motor_control.h>
+#include <refclk.h>
+#include <adc_service.h>
+#include <motion_control_service.h>
+
+#include <xscope.h>
+#include <mc_internal_constants.h>
+
+interface PositionLimiterInterface {
+    void set_limit(int limit);
+    int get_limit();
+};
+
+void demo_motion_control(client interface PositionVelocityCtrlInterface i_position_control);
