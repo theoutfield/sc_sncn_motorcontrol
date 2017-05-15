8d00e0abbb389193b9ed3664cef2f60b2404195c
JauriG <gofre@etud.insa-toulouse.fr>
2017-05-15 10:57:45 +0200

    Build lib_torque_control with xtimecomposer version 14.03

---
+++ lib_bldc_torque_control/include/include/lookup_tables.h
@@ -0,0 +1,15 @@
+/*
+ * lookup_tables.h
+ *
+ *  Created on: 23 March 2017
+ *      Author: Synapticon
+ */
+
+#ifndef LOOKUP_TABLES_H_
+#define LOOKUP_TABLES_H_
+
+extern const short sine_table_control[];
+extern const unsigned short arctg_table_control[];
+extern const unsigned short vectortab_control[];
+
+#endif /* LOOKUP_TABLES_H_ */
