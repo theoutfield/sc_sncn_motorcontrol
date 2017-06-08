50d6697f876ab0381e6871c0272bbe328c9a24a1
Ramin <rsalehi@synapticon.com>
2017-06-08 15:04:33 +0200

    update brake parameters for FS_Robot_II

--- examples/configuration_parameters/user_config.h
+++ examples/configuration_parameters/user_config.h
@@ -163,15 +163,15 @@
 //////////////////////////////////////////////
 //////  BRAKE CONFIGURATION
 //////////////////////////////////////////////
-#define BRAKE_RELEASE_STRATEGY     0    // 0 disabled, 1 normal, 2-100 shaking
+#define BRAKE_RELEASE_STRATEGY     80    // 0 disabled, 1 normal, 2-100 shaking
 #define BRAKE_RELEASE_DELAY        0    // delay in milliseconds between the brake blocking and the stop of the control
 // Voltage which will be applied to electric brake to release (pull) the brake at startup in [milli-Volt].
 // Note: The final voltage (on brake terminals) depends on brake loading characteristics. Generated voltage is precise in the case of pure resistive brake.
-#define PULL_BRAKE_VOLTAGE     13000    // [milli-Volts]
+#define PULL_BRAKE_VOLTAGE     18000    // [milli-Volts]
 // Voltage which will be applied to electric brake to hold the brake after it is pulled [milli-Volt].
 // Note: The final voltage (on brake terminals) depends on brake loading characteristics. Generated voltage is precise in the case of pure resistive brake.
 #define HOLD_BRAKE_VOLTAGE     7000     // [milli-Volts]
-#define PULL_BRAKE_TIME        10000    //Time period in which it is tried to release (pull) the brake [milli seconds]
+#define PULL_BRAKE_TIME        3000    //Time period in which it is tried to release (pull) the brake [milli seconds]
 
 
 /////////////////////////////////////////////////
