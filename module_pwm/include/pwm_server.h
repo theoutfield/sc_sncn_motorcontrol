f25b68cb9496d9df5e63bdd9fff08f8536b7d73c
Ramin <rsalehi@synapticon.com>
2017-07-26 13:47:40 +0200

    update pwm constants

--- module_pwm/include/pwm_server.h
+++ module_pwm/include/pwm_server.h
@@ -27,15 +27,15 @@
  * generate PWM pulses for up to 6 outputs.
  */
 #define GPWM_MAX_VALUE      10000
-#define GPWM_LIMIT_HIGH     10000
-#define DEADTIME            75
+#define GPWM_LIMIT_HIGH      9000
+#define DEADTIME               75
 
 /**
  * @brief Define maximum possible values for general PWM server (which is able to
  * generate PWM pulses for up to 6 outputs.
  */
-#define GPWM_MIN_VALUE      0
-#define GPWM_LIMIT_LOW      0
+#define GPWM_MIN_VALUE         0
+#define GPWM_LIMIT_LOW      1000
 
 ///**
 // * @brief Structure type to define the ports to manage the FET-driver in your IFM SOMANET device (if applicable).
