8992f34525c31d0c5fba977d731d11a225ae2aad
jenkins <jenkins@synapticon.com>
2017-11-24 17:30:56 +0100

    Update BLDC torque control library

--- lib_bldc_torque_control/include/motor_control_structures.h
+++ lib_bldc_torque_control/include/motor_control_structures.h
@@ -153,7 +153,7 @@ typedef struct {
     MotorPhasesConfiguration phases_inverted;   /**< Type of polarity of your motor. */
     int licence;                            /**< Licence number for using the library of module_advanced_foc  */
     SensorType commutation_sensor;          /**< Absolute position sensor used for commutation (if using a BLDC motor). For the moment just Hall sensor can be used [HALL_SENSOR]. */
-    int ifm_tile_usec;
+    int tile_usec;
     int hall_offset[2];                     /**< Feedback Hall sensor error offset for positive (hall_offset[0]) and negative (hall_offset[1]) turning [0:4095]. (Often required to optimize commutation if using a BLDC motor). */
     int hall_state[6];                       /**< Hall port state while being in sector [1-6] */
 
